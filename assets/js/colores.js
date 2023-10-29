22a2779ef8e7b2c9c3c5c555ae4ac5217e60c459
Edison <aalevenegass@gmail.com>
2023-10-29 00:45:11 -0300

    Cambiando funciones en desafío 4

--- assets/js/colores.js
+++ assets/js/colores.js
@@ -1,19 +1,35 @@
-div_1 = document.querySelector("#div_1");
+function cambiarColorFondo(divId) {
+  ele = document.querySelector("#" + divId);
+  ele.style.backgroundColor = "black";
+}
+ele = document.getElementById("div_1");
+ele.addEventListener("click", () => cambiarColorFondo("div_1"));
+
+ele = document.getElementById("div_2");
+ele.addEventListener("click", () => cambiarColorFondo("div_2"));
+
+ele = document.getElementById("div_3");
+ele.addEventListener("click", () => cambiarColorFondo("div_3"));
+
+ele = document.getElementById("div_4");
+ele.addEventListener("click", () => cambiarColorFondo("div_4"));
+
+/* const div_1 = document.querySelector("#div_1");
 div_1.addEventListener("click", () => {
   div_1.style.backgroundColor = "black";
 });
 
-div_2 = document.querySelector("#div_2");
+const div_2 = document.querySelector("#div_2");
 div_2.addEventListener("click", () => {
   div_2.style.backgroundColor = "black";
 });
 
-div_3 = document.querySelector("#div_3");
+const div_3 = document.querySelector("#div_3");
 div_3.addEventListener("click", () => {
   div_3.style.backgroundColor = "black";
 });
 
-div_4 = document.querySelector("#div_4");
+const div_4 = document.querySelector("#div_4");
 div_4.addEventListener("click", () => {
   div_4.style.backgroundColor = "black";
-});
+}); */
