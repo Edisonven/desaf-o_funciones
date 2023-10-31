144a65caa0a25d240bb6fe05eb4ce0c6a22c1881
Edison <aalevenegass@gmail.com>
2023-10-31 13:24:12 -0300

    Reduciendo código de último desafío

--- assets/js/eventos_teclado.js
+++ assets/js/eventos_teclado.js
@@ -1,48 +1,25 @@
-pinkColor = function () {
+bgColor = function (color) {
   let div = document.getElementById("key");
-  div.style.backgroundColor = "pink";
+  div.style.backgroundColor = color;
 };
-orangeColor = function () {
-  let div = document.getElementById("key");
-  div.style.backgroundColor = "orange";
-};
-skyBlueColor = function () {
-  let div = document.getElementById("key");
-  div.style.backgroundColor = "skyblue";
-};
-function nuevoDivMorado() {
+function newElement(color) {
   let container = document.getElementById("container");
   nuevoElemento = document.createElement("div");
   nuevoElemento.style.height = "200px";
   nuevoElemento.style.width = "200px";
-  nuevoElemento.style.backgroundColor = "purple";
+  nuevoElemento.style.backgroundColor = color;
   container.appendChild(nuevoElemento);
 }
-function nuevoDivGris() {
-  let container = document.getElementById("container");
-  nuevoElemento = document.createElement("div");
-  nuevoElemento.style.height = "200px";
-  nuevoElemento.style.width = "200px";
-  nuevoElemento.style.backgroundColor = "Gray";
-  container.appendChild(nuevoElemento);
-}
-function nuevoDivCafe() {
-  let container = document.getElementById("container");
-  nuevoElemento = document.createElement("div");
-  nuevoElemento.style.height = "200px";
-  nuevoElemento.style.width = "200px";
-  nuevoElemento.style.backgroundColor = "brown";
-  container.appendChild(nuevoElemento);
-}
+
 document.addEventListener(
   "keydown",
   (seleccionarTecla = function (evento) {
     if (evento.key === "a" || evento.key === "A") {
-      pinkColor();
+      bgColor("pink");
     } else if (evento.key === "s" || evento.key === "S") {
-      orangeColor();
+      bgColor("orange");
     } else if (evento.key === "d" || evento.key === "D") {
-      skyBlueColor();
+      bgColor("skyblue");
     }
   })
 );
@@ -50,11 +27,11 @@ document.addEventListener(
   "keydown",
   (seleccionarTecla = function (evento) {
     if (evento.key === "q" || evento.key === "Q") {
-      nuevoDivMorado();
+      newElement("purple");
     } else if (evento.key === "w" || evento.key === "W") {
-      nuevoDivGris();
+      newElement("gray");
     } else if (evento.key === "e" || evento.key === "E") {
-      nuevoDivCafe();
+      newElement("brown");
     }
   })
 );
