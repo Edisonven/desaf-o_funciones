13a916da5d741340e89bc2a71a7246af41bcd1c5
Edison <aalevenegass@gmail.com>
2023-10-31 13:42:25 -0300

    Creando función para eliminar el último elemento agregado

--- assets/js/eventos_teclado.js
+++ assets/js/eventos_teclado.js
@@ -1,25 +1,33 @@
-bgColor = function (color) {
+changeBgColor = function (color) {
   let div = document.getElementById("key");
   div.style.backgroundColor = color;
 };
-function newElement(color) {
+function addNewElement(color) {
   let container = document.getElementById("container");
   nuevoElemento = document.createElement("div");
   nuevoElemento.style.height = "200px";
   nuevoElemento.style.width = "200px";
+  nuevoElemento.style.border = "1px solid black";
   nuevoElemento.style.backgroundColor = color;
   container.appendChild(nuevoElemento);
+}
+function eraseNewElement() {
+  container = document.getElementById("container");
+  elementos = container.querySelectorAll("div");
+  if (elementos.length > 1) {
+    container.removeChild(elementos[elementos.length - 1]);
+  }
 }
 
 document.addEventListener(
   "keydown",
   (seleccionarTecla = function (evento) {
     if (evento.key === "a" || evento.key === "A") {
-      bgColor("pink");
+      changeBgColor("pink");
     } else if (evento.key === "s" || evento.key === "S") {
-      bgColor("orange");
+      changeBgColor("orange");
     } else if (evento.key === "d" || evento.key === "D") {
-      bgColor("skyblue");
+      changeBgColor("skyblue");
     }
   })
 );
@@ -27,11 +35,13 @@ document.addEventListener(
   "keydown",
   (seleccionarTecla = function (evento) {
     if (evento.key === "q" || evento.key === "Q") {
-      newElement("purple");
+      addNewElement("purple");
     } else if (evento.key === "w" || evento.key === "W") {
-      newElement("gray");
+      addNewElement("gray");
     } else if (evento.key === "e" || evento.key === "E") {
-      newElement("brown");
+      addNewElement("brown");
+    } else if (evento.key === "f" || evento.key === "F") {
+      eraseNewElement();
     }
   })
 );
