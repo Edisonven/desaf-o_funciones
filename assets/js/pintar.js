76205b304291b5281069e2c75c468fadf73e70d8
Edison <aalevenegass@gmail.com>
2023-11-02 17:43:38 -0300

    Cambiando función en desafío pintar

--- assets/js/pintar.js
+++ assets/js/pintar.js
@@ -2,13 +2,15 @@ function pintar(color) {
   const ele = document.getElementById("ele1");
   ele.style.backgroundColor = color;
 }
+
 pintar("green");
 
-const ele = document.getElementById("ele1");
-const ChangeBgColorOfAnElement = function (color) {
+ChangeBgColorOfAnElement = function (elementId, color) {
+  const ele = document.getElementById(elementId);
   ele.style.backgroundColor = color;
 };
 
+const ele = document.getElementById("ele1");
 ele.addEventListener("click", () => {
-  ChangeBgColorOfAnElement("yellow");
+  ChangeBgColorOfAnElement("ele1", "yellow");
 });
